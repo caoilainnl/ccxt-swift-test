1a61c9975db4ca619aa4d99f3dde9324a86a09ce
caoilainnl <caoilainn.lynch@proton.me>
2025-06-12 20:40:54 -0600

    Initial Commit

---
+++ test-ccxt-cocoapod/test_ccxt_cocoapodApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_ccxt_cocoapodApp.swift
+//  test-ccxt-cocoapod
+//
+//  Created by Caoilainn on 2025-06-12.
+//
+
+import SwiftUI
+
+@main
+struct test_ccxt_cocoapodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
